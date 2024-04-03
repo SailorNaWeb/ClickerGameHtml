de6c1836a7a47210954fe8638a9912568b4f2c97
SailorNaWeb <rafa.juck@outlook.com>
2024-04-03 14:55:35 -0300

    version 1

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 let numClicks = 0
+let velocity = 1
+let priceUp1 = 100
 
 function sumClick() {
 
-	numClicks = numClicks + 1
+	numClicks = numClicks + velocity
+	document.getElementById("counter").innerHTML = "Clicks: " + numClicks
+
+}
+
+function upgradeOne() {
+	if (numClicks > priceUp1) {
+		velocity++
+		numClicks = numClicks - priceUp1
+		priceUp1 = priceUp1 * 2
+		document.getElementById("counter").innerHTML = "Clicks: " + numClicks
+	}
 }
